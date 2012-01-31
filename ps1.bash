909b1d84884c77bc43ccf2792966e7556b18470e
Tom JL Hope <tjlhope@gmail.com>
2012-01-31 11:27:33 +0000

    Change 'ps1.bash' to work on vts as well.
    Incorporate the unicode_start script when running in a 'linux' vt, and
    if there is not unicode support, use a fall back.

--- ps1.bash
+++ ps1.bash
@@ -2,15 +2,22 @@
 ###############################
 ## Customisation of bash PS1
 
+# If on a LANG is utf8, we want unicode support for "…" in `_W`.
+[[ "${LANG}" =~ [Uu][Tt][Ff][-_]?8 ]] && {
+    {	# If on a vt we need to successfully enable unicode
+	[[ "${TERM}" != 'linux' ]] || unicode_start
+    } && _elip="…"	# then enable utf8 elipsis.
+} || {
+    _elip='..'		# replacement string to if no unicode
+}
+
 ## Short version of \w - attempt to limit PWD to set length.
 _W () {
     local wd="${PWD/#${HOME}/~}"	# CWD with ~ for $HOME
-    local len=${1:-30}			# max length of \w
+    local len=${1:-$(( (${COLUMNS} / 2) - 25))}	# max length of \w; def: ~1/2
     #local len=$((${COLUMNS:-80} / 2 - 25))
-    #local rep=".."	# replacement string to indicate shriking
-    local rep="…"	# for this term needs same encoding as file (utf8)
     local chars=1	# minimum number of characters to keep from name
-    local fixdot=1	# non-zero to have ${chars} after . in .dirs
+    local fixdot=1	# non-zero to have ${chars} after . in hidden .dirs
     local iter=3	# incrementally decrement len to ${chars}...
     local sep=2		# ... using decrement of ${sep}
     local nchars=$((${chars}+${iter}*${sep}))	# chars depending on ${iter}
@@ -26,7 +33,7 @@ _W () {
 	    local nc=$((${nchars}+1)) || local nc=${nchars}
 	b="${b#${d}/}"		# body (after current dir)
 	# Iterate over directories for directory to shrink
-	while [ "${d}" != "${b}" -a ${#d} -le $((${nc}+${#rep})) ]
+	while [ "${d}" != "${b}" -a ${#d} -le $((${nc}+${#_elip})) ]
 	do			# if current directory too short
 	    nb="${nb}/${d}"	# add it to new body
 	    d="${b%%/*}"	# get next directory
@@ -44,7 +51,7 @@ _W () {
 		break			# ... done all we can, so end
 	}
 	# Join with reduced dir for new CWD
-	wd="${h}/${nb#/}${nb:+/}${d:0:${nc}}${rep}/${b}"
+	wd="${h}/${nb#/}${nb:+/}${d:0:${nc}}${_elip}/${b}"
     done
     echo "${wd}"
 }
@@ -97,14 +104,17 @@ _R () {
 }
 
 # If run as a program, output a representation of PS1
-[ "${0##*/}" = "ps1.sh" ] && {
+[ "${0##*/}" = "ps1.bash" ] && {
+    
+    [ -n "${PS1}" ] ||		# Give PS1 a default
+	PS1='(example)	\[\033[01;32m\]\u@\h\[\033[01;34m\] $(_W)\[\033[00;36m\]$(_R) \[\033[01;34m\]\$\[\033[00m\] '
     [ "${1}" = "raw" ] &&
-    echo "PS1:	${PS1}" ||
-    echo -e "PS1:\t$(echo "${PS1}" | sed -e 's:\\\(\[\|\]\)::g' \
-					-e "s:\\\\u:$USER:g" \
-					-e "s:\\\\h:$(hostname):g" \
-					-e "s:\$(_W):$(_W):g" \
-					-e "s:\$(_R):$(_R):g" \
-					-e 's:\\\$:$:g' \
-		    )"
+	echo "PS1:	${PS1}" ||
+	echo -e "PS1:\t$(echo "${PS1}" | sed -e 's:\\\(\[\|\]\)::g' \
+					    -e "s:\\\\u:$USER:g" \
+					    -e "s:\\\\h:$(hostname):g" \
+					    -e "s:\$(_W):$(_W):g" \
+					    -e "s:\$(_R):$(_R):g" \
+					    -e 's:\\\$:$:g' \
+			)"
 }
