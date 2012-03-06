fcb1ad21b0e8d389ce518e97574369707a181b5e
Tom JL Hope <tjlhope@gmail.com>
2012-03-06 01:00:16 +0000

    Shell config update.
    bashrc
        small tidy (_dot definition location, ps1.bash sourcing)
    function.sh
        add pd (pushd|popd) function
        fix {cpu,mem}_usage (variable scope)
        add pc_{v,h}block (displays percentage using unicode blocks)
        add basic cpu_mem_pc function to display usages using blocks
    complete.bash
        rearange
        add _cdd completion to pushd and pd

--- complete.bash
+++ complete.bash
@@ -16,37 +16,6 @@ type 'VisualBoyAdvance' > /dev/null 2>&1 &&
 type 'desmume' > /dev/null 2>&1 &&
     complete -o dirnames -fX '!*.[Nn][Dd][Ss]' desmume desmume-glade desmume-cli
 
-# Wrap all alias definitions to allow completion when using them, this
-# dynamically resolves the function - slower, but less environment pollution.
-_wrap_alias () {
-    # Function to generate wrapper
-    local name="${1}"	; shift
-    local cmd="${1}"	; shift
-    [ "${name}" = "${cmd}" ] &&	# don't process ones like ls='ls --color=auto'
-	return 0
-    local ns="_alias."	# namespace for aliases
-    # Get the old completion function and completion line for the new alias
-    local comp="$(complete -p ${cmd} 2>/dev/null | sed -ne \
-	"s:^\(complete\s.*\s-F\s\+\)\(\S\+\)\s\(.*\)${cmd}\s*$:\2|\1${ns}${name} \3${name}:p")"
-    [ -z "${comp}" ] &&		# no completion function
-	return 0
-    local comp_func="${comp%%|complete*}"	# Get original function name
-    # Generate the new wrapper function
-    eval "
-${ns}${name} () {
-    COMP_CWORD=\$(( \${COMP_CWORD} + ${#} ))
-    COMP_WORDS=( ${cmd} ${@} \${COMP_WORDS[@]:1} )
-    ${comp_func} ${cmd} \${COMP_WORDS[\${COMP_CWORD}]} \${COMP_WORDS[\$((\${COMP_CWORD} - 1))]} 
-}"
-    # Generate new complete
-    eval "${comp#*|}"
-}
-# Process all aliases
-eval "$(alias -p | sed -ne \
-    "s:^alias\s\+\([^\:;=]\+\)='\([^;\:\"'(){}]\+\)'\s*$:_wrap_alias \1 \2:p")"
-# don't need function anymore
-unset _wrap_alias
-
 # Completion function allowing 'cd' to interpret N '.'s to mean the (N-1)th
 # parent directory; i.e. '..' is up to parent, '...' is parent's parent,
 # '....' is parent's parent's parent, etc.
@@ -58,8 +27,11 @@ _cdd () {
     _cd $1 ${COMP_WORDS[1]} $2
     return 0
 }
-complete -p cd >/dev/null 2>&1 &&
+complete -p cd >/dev/null 2>&1 && {
     complete -o nospace -F _cdd cd
+    complete -o nospace -F _cdd pushd		# give 'pushd' 'cd' completion
+    complete -o nospace -F _cdd pd		# ... and my 'pd' function
+}
 
 # More complete 'sudo' completion (only works completely with my inputrc and in
 # in a tmux session)
@@ -110,6 +82,39 @@ complete -p cd >/dev/null 2>&1 && {
     alias sudo='sudo '		# needed for alias expansion	# FIXME: HACK!!
     complete -F _sudo sudo
 }
+
+# Wrap all alias definitions to allow completion when using them, generating 
+# individual completion wrapper for each alias (more environment polution but 
+# each individual completion is faster), a 'namespace' is used to make 
+# pollution less obvious.
+_wrap_alias () {
+    # Function to generate wrapper
+    local name="${1}"	; shift
+    local cmd="${1}"	; shift
+    [ "${name}" = "${cmd}" ] &&	# don't process ones like ls='ls --color=auto'
+	return 0
+    local ns="_alias."	# namespace for aliases
+    # Get the old completion function and completion line for the new alias
+    local comp="$(complete -p ${cmd} 2>/dev/null | sed -ne \
+	"s:^\(complete\s.*\s-F\s\+\)\(\S\+\)\s\(.*\)${cmd}\s*$:\2|\1${ns}${name} \3${name}:p")"
+    [ -z "${comp}" ] &&		# no completion function
+	return 0
+    local comp_func="${comp%%|complete*}"	# Get original function name
+    # Generate the new wrapper function
+    eval "
+${ns}${name} () {
+    COMP_CWORD=\$(( \${COMP_CWORD} + ${#} ))
+    COMP_WORDS=( ${cmd} ${@} \${COMP_WORDS[@]:1} )
+    ${comp_func} ${cmd} \${COMP_WORDS[\${COMP_CWORD}]} \${COMP_WORDS[\$((\${COMP_CWORD} - 1))]} 
+}"
+    # Generate new complete
+    eval "${comp#*|}"
+}
+# Process all aliases
+eval "$(alias -p | sed -ne \
+    "s:^alias\s\+\([^\:;=]\+\)='\([^;\:\"'(){}]\+\)'\s*$:_wrap_alias \1 \2:p")"
+# don't need function anymore
+unset _wrap_alias
 
 ## End completions
 ######################
--- function.sh
+++ function.sh
@@ -3,35 +3,143 @@
 ###############################
 ## Definition of shell functions
 
+pd () {
+    # Combine pushd and popd, empty dir, or '-' is a pop, otherwise push.
+    if [ -z "${1}" -o "${1}" = '-' ]
+    then
+	popd
+    else
+	pushd "${@}"
+    fi
+}
+
 pager () {
     [ -n "${*}" ] &&
 	"${@}" | ${PAGER:-less} ||
-	${PAGER:-less}
+	${PAGER:-less} "${@}"
 }
 
 ls_pager () {
     ls "${@}" | ${PAGER:-less}
 }
 
+pc_vblock () {	# FIXME: Rounding Errors
+    # Output a single unicode block character to (vertically) represent a 
+    # percentage. Return values: 0 = OK; 1 = Out of Bounds; 2 = Invalid.
+    if [ -z "${1}" ] || [ -n "${1#[0-9]}" -a \
+			    -n "${1#[0-9][0-9]}" -a \
+			    -n "${1#[0-9][0-9][0-9]}" ]
+    then
+	return 2
+    elif [ ${1} -lt 0 ]; then	# Out of Bounds
+	return 1
+    elif [ ${1} -lt 6 ]; then	# < 6.25
+	echo " "	# <space>
+    elif [ ${1} -le 18 ]; then	# < 18.75
+	echo "▁"	# u2881
+    elif [ ${1} -lt 31 ]; then	# < 31.25
+	echo "▂"	# u2882
+    elif [ ${1} -le 43 ]; then	# < 43.75
+	echo "▃"	# u2583
+    elif [ ${1} -lt 56 ]; then	# < 56.25
+	echo "▄"	# u2584
+    elif [ ${1} -le 68 ]; then	# < 68.75
+	echo "▅"	# u2585
+    elif [ ${1} -lt 81 ]; then	# < 81.25
+	echo "▆"	# u2586
+    elif [ ${1} -le 93 ]; then	# < 93.75
+	echo "▇"	# u2587
+    elif [ ${1} -le 100 ]; then	# <= 100
+	echo "█"	# u2588
+    else			# Out of Bounds
+	return 1
+    fi
+}
+
+pc_hblock () {	# FIXME: Rounding Errors
+    # Output ${2:-4} unicode block characters to (horisontally) represent a 
+    # percentage. Return values: 0 = OK; 1 = Out of Bounds; 2 = Invalid.
+    local p w n i hblock
+    if [ -z "${1}" ] || [ -n "${1#[0-9]}" -a \
+			    -n "${1#[0-9][0-9]}" -a \
+			    -n "${1#[0-9][0-9][0-9]}" ]
+    then
+	return 2
+    else
+	p="${1}"
+    fi
+    if [ -n "${2#[0-9]}" -a \
+	-n "${2#[0-9][0-9]}" -a \
+	-n "${2#[0-9][0-9][0-9]}" ]
+    then
+	return 2
+    else
+	w="${2:-4}"
+    fi
+    n=$(( 100 / ${w} ))
+    while [ ${i:=${n}} -lt ${p} ]
+    do
+	hblock="${hblock}█"	# u2588
+	i=$(( ${i} + ${n} ))
+    done
+    p=$(( (${p} - (${i} - ${n})) * ${w} ))
+    if [ ${p} -lt 0 ]; then	# Out of Bounds
+	return 1
+    elif [ ${p} -lt 6 ]; then	# < 6.25
+	hblock="${hblock} "	# <space>
+    elif [ ${p} -le 18 ]; then	# < 18.75
+	hblock="${hblock}▏"	# u258F
+    elif [ ${p} -lt 31 ]; then	# < 31.25
+	hblock="${hblock}▎"	# u258E
+    elif [ ${p} -le 43 ]; then	# < 43.75
+	hblock="${hblock}▍"	# u258D
+    elif [ ${p} -lt 56 ]; then	# < 56.25
+	hblock="${hblock}▌"	# u258C
+    elif [ ${p} -le 68 ]; then	# < 68.75
+	hblock="${hblock}▋"	# u258B
+    elif [ ${p} -lt 81 ]; then	# < 81.25
+	hblock="${hblock}▊"	# u258A
+    elif [ ${p} -le 93 ]; then	# < 93.75
+	hblock="${hblock}▉"	# u2589
+    elif [ ${1} -le 100 ]; then	# <= 100
+	hblock="${hblock}█"	# u2588
+    else			# Out of Bounds
+	return 1
+    fi
+    i=$(( ${i} + ${n} ))
+    while [ ${i} -lt $(( 100 + ${n} )) ]
+    do
+	hblock="${hblock} "	# <space>
+	i=$(( ${i} + ${n} ))
+    done
+    echo "${hblock}"
+}
+
 cpu_usage () {
-    # Get the total cpu line from /proc/stat: "IDLE|T1 T2 T3 T4 IDLE T5 ... TN"
+    # Get the total cpu line from /proc/stat: "IDLE|T1 T2 T3 IDLE T5 ... TN"
+    local cpu v a t p
     cpu="$(sed -n /proc/stat -e \
-	's/^cpu\s\+\(\([0-9]\+\s\+\)\{4\}\)\([0-9]\+\)\(.*\)$/\3|\1\3\4/p')"
+	's/^cpu\s\+\(\([0-9]\+\s\+\)\{3\}\)\([0-9]\+\)\(.*\)$/\3|\1\3\4/p')"
     # Sum all the values after the bar to get the total
+    [ -f "${SHM}/cpu" ] &&
+	read _a _t < "${SHM}/cpu"
     for v in ${cpu#*|}
     do
 	t=$(( ${t-0} + ${v}))			# total time (cs)
     done
     a=$(( ${t} - ${cpu%%|*} ))			# active time (cs)
-    p=$(( (1000 * ${a} / ${t} + 5) / 10 ))	# percent use
+    # percent use
+    p=$(( (1000 * (${a} - ${_a:-0}) / (${t} - ${_t:-0}) + 5) / 10 ))
     # Output the percentage
     [ "${1}" = '-v' ] &&
-	echo "${a} / ${t} = ${p}" ||
+	echo "(${a} - ${_a-0}) / (${t} - ${_t-0}) = ${p}" ||
 	echo "${p}"
+    echo "${a} ${t}" > "${SHM}/cpu"
 }
 
 mem_usage () {
     # Get the total and active mem values from /proc/meminfo: ACTIVE|TOTAL
+    local mem v a t p
     mem="$(sed -n /proc/meminfo -e \
 	':new
 	N
@@ -44,6 +152,14 @@ mem_usage () {
     [ "${1}" = '-v' ] &&
 	echo "${a} / ${t} = ${p}" ||
 	echo "${p}"
+}
+
+cpu_mem_pc () {
+    # Output a block representation of cpu and memory.
+    [ -n "${1}" ] && [ ${1} -gt 1 ] &&
+	local pc_block="pc_hblock" ||
+	local pc_block="pc_vblock"
+    echo "$(${pc_block} "$(cpu_usage)" ${1})$(${pc_block} "$(mem_usage)" ${1})"
 }
 
 ## End functions
