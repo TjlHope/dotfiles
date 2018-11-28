a5c274acfef60f531f0f4d5998a0cd0281187433
Tom JL Hope <Tom.Hope@telsis.com>
2018-11-28 11:10:03 +0000

    Add generalised ssh config
    - ssh config is done using:
      - a top level Include config.d/*.conf
      - a .ssh/config.d dir for host based config
      - and a .ssh/snippet.d dir for shared options
      it includes my Control shared pipe based settings.
    - Modified shm_d.sh to setup /tmp/$USER as well, so it's easily usable
      for ssh Control.
    - Modified ignore files to better include .d directories.

--- rc.d/shm_d.sh
+++ rc.d/shm_d.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
-# ~/.profile.d/shell_name.sh
-# Setup the ${SHM_D} env var (user temporary storage)
+# shellcheck disable=2015
+# ~/.profile.d/shm_d.sh
+# Setup the $TMP_D and $SHM_D env vars (user temporary storage)
+
+export TMP_D="/tmp/$USER"
 
 { [ -f /proc/mounts ] && cat /proc/mounts || mount; } |
-    egrep -q '^\s*\S+\s+/dev/shm\s+tmpfs\s+' &&
-    export SHM_D="/dev/shm/${USER}" ||
-    export SHM_D="/tmp/${USER}"	# default to /tmp if no shared memory
+    grep -Eq '^\s*\S+\s+/dev/shm\s+tmpfs\s+' &&
+    export SHM_D="/dev/shm/$USER" ||
+    export SHM_D="$TMP_D"	# default to /tmp if no shared memory
 
-{
-    [ -d "${SHM_D}" ] || mkdir "${SHM_D}"
-} && {			#^ no -p as /dev/shm and /tmp must exist
+{		# don't use mkdir -p as /dev/shm and /tmp must exist
+    [ -d "$SHM_D" ] || mkdir "$SHM_D"
+} && {		# make the dir user read only and sticky
     [ "$(stat -c'%Mp%Lp' "$SHM_D")" = "1700" ] || chmod 1700 "$SHM_D"
-} || {					#^ user read only and sticky
+} && {		# if $TMP_D doesn't already exist, symlink it to SHM_D
+    [ -d "$TMP_D" ] || ln -s "$SHM_D" "$TMP_D"
+} || {
+    # If we've had a failure, always unset $SHM_D as it might not have the
+    # permissions we need. But only unset $TMP_D if it doesn't exist.
     unset SHM_D
+    [ -d "$TMP_D" ] ||
+	{ [ -h "$TMP_D" ] && [ -d "$(readlink "$SHM_D")" ]; } ||
+	unset TMP_D
     false
 }
